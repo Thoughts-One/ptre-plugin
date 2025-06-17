f1e129420e3bf47f9224efc50eeaea7b728cf0a2
Kai Fickenscher <kai.fickenscher@gmail.com>
2025-06-17 15:29:33 +0200

    corrected path of lass-ptre-functions.php in /wp-content/plugins/ptre-plugin/inc

--- inc/class-ptre-functions.php
+++ inc/class-ptre-functions.php
@@ -1,285 +1,158 @@
-<?php
-/**
- * PTRE Plugin Functions
- * 
- * Contains functions migrated from peter-thompson/functions.php
- */
-
-if (!defined('ABSPATH')) {
-    exit;
-}
-
-class PTRE_Functions {
-    public function __construct() {
-        // Initialize all hooks
-        $this->init_hooks();
-    }
-
-    private function init_hooks() {
-        // Schema and SEO
-        add_filter('wpseo_json_ld_output', [$this, 'add_real_estate_schema_to_yoast'], 20);
-        add_filter('wpseo_json_ld_output', [$this, 'add_aggregate_ratings'], 20);
-        add_filter('wpseo_canonical', [$this, 'fix_property_canonical'], 99);
-        add_action('wp_head', [$this, 'output_property_canonical'], 1);
-        
-        // Multilingual
-        add_action('wp_head', [$this, 'fix_property_hreflang_tags'], 0);
-        add_filter('wpml_hreflangs', [$this, 'remove_xdefault_hreflang'], 20);
-        
-        // Rewrite Rules
-        add_action('init', [$this, 'add_property_rewrite_rules']);
-        
-        // Admin
-        add_action('admin_init', [$this, 'disable_comments_admin']);
-        add_action('admin_menu', [$this, 'remove_comments_menu']);
-        add_action('init', [$this, 'remove_comments_admin_bar']);
-        add_filter('comments_open', '__return_false', 20, 2);
-        add_filter('pings_open', '__return_false', 20, 2);
-        add_filter('comments_array', '__return_empty_array', 10, 2);
-        
-        // ACF
-        add_action('acf/init', [$this, 'add_acf_options_page']);
-        
-        // Author Fields
-        add_action('show_user_profile', [$this, 'add_author_phone_field']);
-        add_action('edit_user_profile', [$this, 'add_author_phone_field']);
-        add_action('personal_options_update', [$this, 'save_author_phone_field']);
-        add_action('edit_user_profile_update', [$this, 'save_author_phone_field']);
-        add_filter('wpseo_schema_person', [$this, 'modify_person_to_author'], 11, 2);
-    }
-
-    /**
-     * Add real estate schema to Yoast JSON-LD
-     */
-    public function add_real_estate_schema_to_yoast($data) {
-        if (is_singular('properties')) {
-            global $post;
-
-            $property_schema = [
-                "@context" => "https://schema.org",
-                "@type" => "RealEstateListing",
-                "@id" => get_permalink() . "#realestate",
-                "mainEntityOfPage" => [
-                    "@id" => get_permalink() . "#webpage"
-                ],
-                "name" => get_the_title(),
-                "url" => get_permalink(),
-                "image" => get_the_post_thumbnail_url(),
-                "description" => get_the_excerpt(),
-                "address" => [
-                    "@type" => "PostalAddress",
-                    "streetAddress" => get_post_meta($post->ID, 'property_address', true),
-                    "addressLocality" => get_post_meta($post->ID, 'property_city', true),
-                    "addressRegion" => "QC",
-                    "postalCode" => get_post_meta($post->ID, 'property_postal_code', true),
-                    "addressCountry" => "CA"
-                ],
-                "geo" => [
-                    "@type" => "GeoCoordinates",
-                    "latitude" => get_post_meta($post->ID, 'property_latitude', true),
-                    "longitude" => get_post_meta($post->ID, 'property_longitude', true)
-                ],
-                "offers" => [
-                    "@type" => "Offer",
-                    "price" => get_post_meta($post->ID, 'property_price', true),
-                    "priceCurrency" => "CAD",
-                    "availability" => "https://schema.org/InStock"
-                ]
-            ];
-
-            $data['@graph'][] = $property_schema;
-        }
-
-        return $data;
-    }
-
-    /**
-     * Handle multilingual property URLs (hreflang tags)
-     */
-    public function fix_property_hreflang_tags() {
-        if (get_post_type() !== 'properties') {
-            return;
-        }
-
-        // Stop WPML's own hreflang tags
-        remove_action('wp_head', ['WPML_SEO_HeadLangs', 'head_langs']);
-
-        $base_url = get_permalink();
-        if (!$base_url) return;
-
-        $langs = [
-            'en' => 'en-CA',
-            'fr' => 'fr-CA',
-        ];
-
-        foreach ($langs as $code => $hreflang) {
-            $url = $code === 'fr' ? $base_url : str_replace('/properties/', '/en/properties/', $base_url);
-            printf('<link rel="alternate" hreflang="%s" href="%s" />' . "\n", $hreflang, esc_url($url));
-        }
-
-        // Add x-default without /x-default/ in the URL
-        printf('<link rel="alternate" href="%s" hreflang="x-default" />' . "\n", esc_url($base_url));
-    }
-
-    /**
-     * Add rewrite rules for property URLs
-     */
-    public function add_property_rewrite_rules() {
-        add_rewrite_rule(
-            'properties/([^/]+)/([0-9]+)/?$',
-            'index.php?post_type=properties&mls=$matches[2]',
-            'top'
-        );
-        add_rewrite_rule(
-            'en/properties/([^/]+)/([0-9]+)/?$',
-            'index.php?post_type=properties&mls=$matches[2]&lang=en',
-            'top'
-        );
-    }
-
-    /**
-     * Add ACF options page
-     */
-    public function add_acf_options_page() {
-        if (function_exists('acf_add_options_page')) {
-            acf_add_options_page('Theme Options');
-        }
-    }
-
-    /**
-     * Disable comments in admin
-     */
-    public function disable_comments_admin() {
-        global $pagenow;
-        
-        if ($pagenow === 'edit-comments.php') {
-            wp_safe_redirect(admin_url());
-            exit;
-        }
-
-        remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
-
-        foreach (get_post_types() as $post_type) {
-            if (post_type_supports($post_type, 'comments')) {
-                remove_post_type_support($post_type, 'comments');
-                remove_post_type_support($post_type, 'trackbacks');
-            }
-        }
-    }
-
-    /**
-     * Remove comments menu
-     */
-    public function remove_comments_menu() {
-        remove_menu_page('edit-comments.php');
-    }
-
-    /**
-     * Remove comments from admin bar
-     */
-    public function remove_comments_admin_bar() {
-        if (is_admin_bar_showing()) {
-            remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
-        }
-    }
-
-    /**
-     * Add aggregate ratings to Yoast JSON
-     */
-    public function add_aggregate_ratings($data) {
-        if (!is_front_page()) return $data;
-
-        foreach ($data['@graph'] as &$node) {
-            if (
-                isset($node['@type']) &&
-                in_array('Organization', (array) $node['@type'], true) &&
-                $node['@id'] === 'https://peterthompson.ca/#organization'
-            ) {
-                $node['aggregateRating'] = [
-                    '@type' => 'AggregateRating',
-                    'ratingValue' => '5.0',
-                    'reviewCount' => '115'
-                ];
-            }
-        }
-
-        return $data;
-    }
-
-    /**
-     * Fix canonical URL for single property pages
-     */
-    public function fix_property_canonical($canonical) {
-        global $result_obj;
-        if (get_query_var('mls') && isset($result_obj[$_GET['mls']]['property'])) {
-            return $this->get_property_permalink((object)$result_obj[$_GET['mls']]['property']);
-        }
-        return $canonical;
-    }
-
-    /**
-     * Output property canonical URL
-     */
-    public function output_property_canonical() {
-        if (get_query_var('mls') && isset($GLOBALS['result_obj'][$_GET['mls']]['property'])) {
-            $canonical = $this->get_property_permalink((object)$GLOBALS['result_obj'][$_GET['mls']]['property']);
-            echo '<link rel="canonical" href="' . esc_url($canonical) . '" />' . PHP_EOL;
-        }
-    }
-
-    /**
-     * Remove WPML's x-default hreflang for property pages
-     */
-    public function remove_xdefault_hreflang($hreflangs) {
-        if (is_singular('properties') || is_post_type_archive('properties')) {
-            unset($hreflangs['x-default']);
-        }
-        return $hreflangs;
-    }
-
-    /**
-     * Get property permalink
-     */
-    public function get_property_permalink($property) {
-        $address = $property->civic_number_start . ' ' . $property->street_name;
-        $slug = urlencode($address);
-        return home_url("/properties/{$slug}/{$property->mls_no}/");
-    }
-
-    /**
-     * Add author schema
-     */
-    public function modify_person_to_author($data, $context) {
-        if (!empty($data['@type'])) {
-            $data['@type'] = ['Author', 'Person'];
-        }
-        return $data;
-    }
-
-    /**
-     * Add the custom phone number field to the user profile page
-     */
-    public function add_author_phone_field($user) { ?>
-        <h3>Contact Information</h3>
-        <table class="form-table">
-            <tr>
-                <th><label for="author_phone">Phone Number</label></th>
-                <td>
-                    <input type="text" name="author_phone" id="author_phone" 
-                           value="<?php echo esc_attr(get_the_author_meta('author_phone', $user->ID)); ?>" 
-                           class="regular-text" />
-                </td>
-            </tr>
-        </table>
-    <?php }
-
-    /**
-     * Save the custom phone number field
-     */
-    public function save_author_phone_field($user_id) {
-        if (!current_user_can('edit_user', $user_id)) {
-            return false;
-        }
-        update_user_meta($user_id, 'author_phone', sanitize_text_field($_POST['author_phone']));
-    }
+<?php  
+/**  
+ * The core plugin class.  
+ *  
+ * This is used to define internationalization,  
+ * admin-specific hooks, and public-facing hooks.  
+ *  
+ * Also maintains the unique identifier of this plugin as well as the current  
+ * version of the plugin.  
+ *  
+ * @package PTRE_Plugin  
+ */  
+class Ptre_Functions {  
+  
+    /**  
+     * The loader that's responsible for maintaining and registering all hooks.  
+     *  
+     * @access   protected  
+     * @var      Ptre_Loader    $loader    Maintains and registers all hooks for the plugin.  
+     */  
+    protected $loader;  
+  
+    /**  
+     * The unique identifier of this plugin.  
+     *  
+     * @access   protected  
+     * @var      string    $plugin_name    The string used to uniquely identify this plugin.  
+     */  
+    protected $plugin_name;  
+  
+    /**  
+     * The current version of the plugin.  
+     *  
+     * @access   protected  
+     * @var      string    $version    The current version of the plugin.  
+     */  
+    protected $version;  
+  
+    /**  
+     * Define the core functionality of the plugin.  
+     *  
+     * Set the plugin name and the plugin version that can be used throughout the plugin.  
+     * Load the dependencies, define the locale, and set the hooks for the admin area and  
+     * the public-facing side of the site.  
+     *  
+     * @since    1.0.0  
+     */  
+    public function __construct() {  
+        $this->plugin_name = 'ptre-plugin';  
+        $this->version = PTRE_PLUGIN_VERSION;  
+  
+        $this->load_dependencies();  
+        $this->define_admin_hooks();  
+        $this->define_public_hooks();  
+    }  
+  
+    /**  
+     * Load the required dependencies for this plugin.  
+     *  
+     * Include the following files that make up the plugin:  
+     *  
+     * - Ptre_Loader. Orchestrates the hooks of the plugin.  
+     * - Ptre_i18n. Defines internationalization functionality.  
+     * - Ptre_Admin. Defines all hooks for the admin area.  
+     * - Ptre_Public. Defines all hooks for the public side of the site.  
+     *  
+     * @access   private  
+     * @since    1.0.0  
+     */  
+    private function load_dependencies() {  
+        // Corrected paths relative to the plugin root  
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-ptre-loader.php';  
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-ptre-i18n.php';  
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-ptre-admin.php';  
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-ptre-public.php';  
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-ptre-custom-post-types.php';  
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-ptre-api-handler.php';  
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-ptre-blocks.php';  
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/blocks/hero-section-block.php'; // Also ensure this path is correct  
+  
+        $this->loader = new Ptre_Loader();  
+    }  
+  
+    /**  
+     * Define the hooks and the callback functions that are used for  
+     * setting up the admin area.  
+     *  
+     * @since    1.0.0  
+     * @access   private  
+     */  
+    private function define_admin_hooks() {  
+        $plugin_admin = new Ptre_Admin( $this->get_plugin_name(), $this->get_version() );  
+  
+        $this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );  
+        $this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );  
+    }  
+  
+    /**  
+     * Define the hooks and the callback functions that are used for  
+     * setting up the public-facing side of the site.  
+     *  
+     * @since    1.0.0  
+     * @access   private  
+     */  
+    private function define_public_hooks() {  
+        $plugin_public = new Ptre_Public( $this->get_plugin_name(), $this->get_version() );  
+  
+        $this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );  
+        $this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );  
+  
+        // Load MW Properties shortcode overrides from the plugin  
+        $this->loader->add_action('plugins_loaded', $this, 'load_mw_properties_shortcodes');  
+    }  
+  
+    /**  
+     * Run the loader to execute all of the hooks with WordPress.  
+     *  
+     * @since    1.0.0  
+     */  
+    public function run() {  
+        $this->loader->run();  
+    }  
+  
+    /**  
+     * The name of the plugin used to uniquely identify it within the context of WordPress and  
+     * to define internationalization functionality.  
+     *  
+     * @since     1.0.0  
+     * @return    string    The name of the plugin.  
+     */  
+    public function get_plugin_name() {  
+        return $this->plugin_name;  
+    }  
+  
+    /**  
+     * Retrieve the version number of the plugin.  
+     *  
+     * @since     1.0.0  
+     * @return    string    The version number of the plugin.  
+     */  
+    public function get_version() {  
+        return $this->version;  
+    }  
+  
+    /**  
+     * Load MW Properties shortcode overrides from the plugin.  
+     *  
+     * @since 1.0.0  
+     */  
+    public function load_mw_properties_shortcodes() {  
+        // Corrected path for shortcodes relative to plugin root  
+        $plugin_shortcode_path = plugin_dir_path( dirname( __FILE__ ) ) . 'mw-properties-templates/shortcodes/';  
+        if (file_exists($plugin_shortcode_path . 'nearby-cities.php')) {  
+            require_once $plugin_shortcode_path . 'nearby-cities.php';  
+        }  
+        // Add other shortcode overrides here if they exist in the plugin  
+    }  
 }
--- ptre-plugin.php
+++ ptre-plugin.php
@@ -1,58 +1,44 @@
-<?php
-/**
- * Plugin Name: PTRE Plugin
- * Plugin URI:  https://peterthompson.ca
- * Description: Companion plugin for the PTRE Theme, handling custom post types, taxonomies, shortcodes, and other non-theme-specific functionalities.
- * Version:     1.0.0
- * Author:      Marketing Websites
- * Author URI:  https://marketingwebsites.ca
- * License:     GPL-2.0+
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain: ptre-plugin
- * Domain Path: /languages
- *
- * @package PTRE_Plugin
- */
-
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-    die;
-}
-
-/**
- * Current plugin version.
- */
-define( 'PTRE_PLUGIN_VERSION', '1.0.0' );
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'includes/class-ptre-functions.php';
-require plugin_dir_path( __FILE__ ) . 'includes/class-ptre-custom-post-types.php';
-require plugin_dir_path( __FILE__ ) . 'includes/class-ptre-api-handler.php';
-require plugin_dir_path( __FILE__ ) . 'includes/class-ptre-blocks.php'; // This file should register the block category
-require plugin_dir_path( __FILE__ ) . 'includes/blocks/hero-section-block.php'; // Include the new block file
-
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file means
- * that all of the hooks will be defined.
- */
-function run_ptre_plugin() {
-    $plugin = new Ptre_Functions();
-    $plugin->run();
-}
+<?php  
+/**  
+ * Plugin Name: PTRE Plugin  
+ * Plugin URI:  https://peterthompson.ca  
+ * Description: Companion plugin for the PTRE Theme, handling custom post types, taxonomies, shortcodes, and other non-theme-specific functionalities.  
+ * Version:     1.0.0  
+ * Author:      Marketing Websites  
+ * Author URI:  https://marketingwebsites.ca  
+ * License:     GPL-2.0+  
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt  
+ * Text Domain: ptre-plugin  
+ * Domain Path: /languages  
+ *  
+ * @package PTRE_Plugin  
+ */  
+  
+// If this file is called directly, abort.  
+if ( ! defined( 'WPINC' ) ) {  
+    die;  
+}  
+  
+/**  
+ * Current plugin version.  
+ */  
+define( 'PTRE_PLUGIN_VERSION', '1.0.0' );  
+  
+/**  
+ * The core plugin class that is used to define internationalization,  
+ * admin-specific hooks, and public-facing hooks.  
+ */  
+require plugin_dir_path( __FILE__ ) . 'inc/class-ptre-functions.php'; // Corrected path  
+  
+/**  
+ * Begins execution of the plugin.  
+ *  
+ * Since everything within the plugin is registered via hooks,  
+ * then kicking off the plugin from this point in the file means  
+ * that all of the hooks will be defined.  
+ */  
+function run_ptre_plugin() {  
+    $plugin = new Ptre_Functions();  
+    $plugin->run();  
+}  
 run_ptre_plugin();
-
-// Load MW Properties shortcode overrides from the plugin
-// This ensures the plugin's shortcode templates are loaded before the theme's, if any.
-add_action('plugins_loaded', function() {
-    $plugin_shortcode_path = plugin_dir_path(__FILE__) . 'mw-properties-templates/shortcodes/';
-    if (file_exists($plugin_shortcode_path . 'nearby-cities.php')) {
-        require_once $plugin_shortcode_path . 'nearby-cities.php';
-    }
-    // Add other shortcode overrides here if they exist in the plugin
-});
